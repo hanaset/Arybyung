823afe786a83a0e1f043426076b06b5d8c6e69fb
Luke <b183523@gmail.com>
2020-01-23 12:22:01 +0900

    Provider 모듈 생성

---
+++ arybyung-provider/src/main/java/com/how/ArybyungProviderApplication.java
@@ -0,0 +1,13 @@
+package com.how;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArybyungProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArybyungProviderApplication.class, args);
+    }
+
+}
---
+++ arybyung-provider/src/test/java/com/how/arybyungprovider/ArybyungProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.how.arybyungprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArybyungProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
